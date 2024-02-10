c0f3d2546180bd137a1e20f60c9b0097afab8178
mfaqihridho <faqih.ridho@orderfaz.com>
2024-02-10 12:29:11 +0700

    ✨ learn textfield

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,21 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:quran_app/main_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController controllerPassword = TextEditingController(text: '');
+  TextEditingController controllerUsername = TextEditingController(text: '');
+
+  var errors = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: ElevatedButton(
-        child: Text("Login"),
-        onPressed: () {
-          Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) {
-            return const MainPage();
-          }));
-        },
+          child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                  errorText:
+                      errors.contains('username') ? 'Invalid username' : null),
+              onChanged: (value) {
+                setState(() {});
+                errors.remove('username');
+              },
+              controller: controllerUsername,
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                errorText:
+                    errors.contains('password') ? 'Invalid password' : null,
+              ),
+              obscureText: true,
+              onChanged: (value) {
+                setState(() {});
+                errors.remove('password');
+              },
+              controller: controllerPassword,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text("Login"),
+              onPressed: () {
+                setState(() {});
+                if (controllerPassword.text == '123' &&
+                    controllerUsername.text == 'admin') {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) {
+                    return const MainPage();
+                  }));
+                } else {
+                  errors.add('username');
+                  errors.add('password');
+                }
+              },
+            ),
+          ],
+        ),
       )),
     );
   }
