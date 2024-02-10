3d8c40ae041f429a074319f0b83879d035472a7a
mfaqihridho <faqih.ridho@orderfaz.com>
2024-02-10 10:52:11 +0700

    ✨ gradient appbar

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/login_page.dart';
 import 'package:quran_app/second_page.dart';
 
 class MainPage extends StatelessWidget {
@@ -8,7 +9,28 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Page'),
+        leading: Icon(Icons.menu),
+        title: Text('Appbar Example'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+          IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) {
+                  return const LoginPage();
+                }));
+              },
+              icon: Icon(Icons.exit_to_app))
+        ],
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xff0096ff), Color(0xff6610f2)],
+              begin: Alignment.topLeft,
+              end: Alignment(0.8, 1),
+            ),
+          ),
+        ),
       ),
       body: Center(
           child: ElevatedButton(
